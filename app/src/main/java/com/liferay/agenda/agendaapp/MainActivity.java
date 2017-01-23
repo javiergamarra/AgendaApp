f5a82e9b481b81b4fa2a3df1ab4dcd404c467725
Javier Gamarra <nhpatt@gmail.com>
2017-01-23 13:27:23 -0800

    using thread and eventbus

--- app/src/main/java/com/liferay/agenda/agendaapp/MainActivity.java
+++ app/src/main/java/com/liferay/agenda/agendaapp/MainActivity.java
@@ -2,10 +2,14 @@ package com.liferay.agenda.agendaapp;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.widget.ListView;
+import java.io.IOException;
 import java.util.List;
+import org.greenrobot.eventbus.EventBus;
+import org.greenrobot.eventbus.Subscribe;
+import org.greenrobot.eventbus.ThreadMode;
 import retrofit2.Call;
-import retrofit2.Callback;
 import retrofit2.Response;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
@@ -17,26 +21,41 @@ public class MainActivity extends AppCompatActivity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 
-		Retrofit retrofit = new Retrofit.Builder().baseUrl("https://api.github.com/")
+		final Retrofit retrofit = new Retrofit.Builder().baseUrl("https://api.github.com/")
 			.addConverterFactory(GsonConverterFactory.create())
 			.build();
 
+		new Thread(new Runnable() {
+			@Override
+			public void run() {
+				GitHubService service = retrofit.create(GitHubService.class);
+				Call<List<Event>> call = service.listRepos("octocat");
+				try {
+					Response<List<Event>> response = call.execute();
+					EventBus.getDefault().post(response);
+				} catch (IOException e) {
+					Log.e("Agenda", "Don't do this");
+				}
+			}
+		}).start();
+	}
+
+	@Override
+	public void onStart() {
+		super.onStart();
+		EventBus.getDefault().register(this);
+	}
+
+	@Override
+	public void onStop() {
+		super.onStop();
+		EventBus.getDefault().unregister(this);
+	}
+
+	@Subscribe(threadMode = ThreadMode.MAIN)
+	public void onMessageEvent(Response<List<Event>> response) {
 		final ListView listView = (ListView) findViewById(R.id.list);
-
-		GitHubService service = retrofit.create(GitHubService.class);
-		Call<List<Event>> call = service.listRepos("octocat");
-		call.enqueue(new Callback() {
-			@Override
-			public void onResponse(Call call, Response response) {
-				EventAdapter adapter =
-					new EventAdapter(MainActivity.this, R.layout.event_row, (List<Event>) response.body());
-				listView.setAdapter(adapter);
-			}
-
-			@Override
-			public void onFailure(Call call, Throwable t) {
-
-			}
-		});
+		EventAdapter adapter = new EventAdapter(MainActivity.this, R.layout.event_row, response.body());
+		listView.setAdapter(adapter);
 	}
 }
