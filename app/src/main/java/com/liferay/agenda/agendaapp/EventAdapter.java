ccb84e255caaec4d8515c659bcbb0a4f4cb6b2d5
Javier Gamarra <nhpatt@gmail.com>
2017-01-23 12:47:41 -0800

    restore click and clean

--- app/src/main/java/com/liferay/agenda/agendaapp/EventAdapter.java
+++ app/src/main/java/com/liferay/agenda/agendaapp/EventAdapter.java
@@ -1,5 +1,6 @@
 package com.liferay.agenda.agendaapp;
 
+import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
@@ -9,7 +10,8 @@ import android.widget.ArrayAdapter;
 import android.widget.TextView;
 import java.util.List;
 
-class EventAdapter extends ArrayAdapter<Event> {
+class EventAdapter extends ArrayAdapter<Event> implements View.OnClickListener {
+
 	public EventAdapter(MainActivity mainActivity, int row, List<Event> list) {
 		super(mainActivity, row, list);
 	}
@@ -17,15 +19,24 @@ class EventAdapter extends ArrayAdapter<Event> {
 	@NonNull
 	@Override
 	public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+
 		if (convertView == null) {
-			convertView = LayoutInflater.from(getContext()).inflate(R.layout.row, parent, false);
+			convertView = LayoutInflater.from(getContext()).inflate(R.layout.event_row, parent, false);
 		}
-		TextView hour = (TextView) convertView.findViewById(R.id.hour);
+		TextView hour = (TextView) convertView.findViewById(R.id.event_hour);
 		Event event = getItem(position);
 		hour.setText(event.getHour());
 
-		TextView text = (TextView) convertView.findViewById(R.id.text);
+		TextView text = (TextView) convertView.findViewById(R.id.event_text);
 		text.setText(event.getName());
+
+		convertView.findViewById(R.id.image_button).setOnClickListener(this);
+
 		return convertView;
+	}
+
+	@Override
+	public void onClick(View v) {
+		getContext().startActivity(new Intent(getContext(), DetailActivity.class));
 	}
 }
--- app/src/main/java/com/liferay/agenda/agendaapp/MainActivity.java
+++ app/src/main/java/com/liferay/agenda/agendaapp/MainActivity.java
@@ -1,61 +1,23 @@
 package com.liferay.agenda.agendaapp;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
-import android.view.View;
-import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import java.util.ArrayList;
 import java.util.List;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
-
-	private boolean state;
+public class MainActivity extends AppCompatActivity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 
-		//ImageButton imageButton = (ImageButton) findViewById(R.id.image_button);
-		//imageButton.setOnClickListener(this);
-		//
-		//View row = findViewById(R.id.row);
-		//row.setOnClickListener(this);
-
 		ListView listView = (ListView) findViewById(R.id.list);
-		List<Event> list = new ArrayList();
-		list.add(new Event("09:00", "Android Workshop"));
-		list.add(new Event("09:30", "SemVer"));
-		listView.setAdapter(new EventAdapter(this, R.layout.row, list));
-	}
-
-	@Override
-	protected void onResume() {
-		super.onResume();
-
-		Log.d("Agenda", "State: " + state);
-	}
-
-	@Override
-	public void onClick(View v) {
-		state = true;
-		if (v.getId() == R.id.row) {
-			startActivity(new Intent(this, DetailActivity.class));
-		}
-	}
-
-	@Override
-	protected void onSaveInstanceState(Bundle outState) {
-		outState.putBoolean("state", state);
-		super.onSaveInstanceState(outState);
-	}
-
-	@Override
-	protected void onRestoreInstanceState(Bundle savedInstanceState) {
-		super.onRestoreInstanceState(savedInstanceState);
-		state = savedInstanceState.getBoolean("state");
+		List<Event> events = new ArrayList<>();
+		events.add(new Event("09:00", "Android Workshop"));
+		events.add(new Event("09:30", "SemVer"));
+		EventAdapter adapter = new EventAdapter(this, R.layout.event_row, events);
+		listView.setAdapter(adapter);
 	}
 }
