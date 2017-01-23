b1d945fd153ba2e19ffb2edfdc6ed3ef93b0d00b
Javier Gamarra <nhpatt@gmail.com>
2017-01-23 11:04:35 -0800

    ids

--- app/src/main/java/com/liferay/agenda/agendaapp/MainActivity.java
+++ app/src/main/java/com/liferay/agenda/agendaapp/MainActivity.java
@@ -2,6 +2,7 @@ package com.liferay.agenda.agendaapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,8 @@ public class MainActivity extends AppCompatActivity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+
+		ImageButton imageButton = (ImageButton) findViewById(R.id.image_button);
+		imageButton.setBackgroundColor(getResources().getColor(R.color.colorPrimary));
 	}
 }
