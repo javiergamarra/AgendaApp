d5b3256200d07f039207b13c950baec0715e58dd
Javier Gamarra <nhpatt@gmail.com>
2017-01-23 14:07:07 -0800

    trying retrofit

--- app/src/main/java/com/liferay/agenda/agendaapp/MainActivity.java
+++ app/src/main/java/com/liferay/agenda/agendaapp/MainActivity.java
@@ -2,10 +2,15 @@ package com.liferay.agenda.agendaapp;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Base64;
 import android.util.Log;
 import android.widget.ListView;
 import java.io.IOException;
 import java.util.List;
+import okhttp3.Interceptor;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.ResponseBody;
 import org.greenrobot.eventbus.EventBus;
 import org.greenrobot.eventbus.Subscribe;
 import org.greenrobot.eventbus.ThreadMode;
@@ -21,7 +26,28 @@ public class MainActivity extends AppCompatActivity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 
-		final Retrofit retrofit = new Retrofit.Builder().baseUrl("https://api.github.com/")
+		//final Retrofit retrofit = new Retrofit.Builder().baseUrl("https://api.github.com/")
+		//	.addConverterFactory(GsonConverterFactory.create())
+		//	.build();
+
+		OkHttpClient.Builder builder = new OkHttpClient.Builder();
+		builder.addInterceptor(new Interceptor() {
+			@Override
+			public okhttp3.Response intercept(Chain chain) throws IOException {
+
+				String authorizationString =
+					"Basic " + Base64.encodeToString(("test@liferay.com:test").getBytes(), Base64.NO_WRAP);
+				Request request = chain.request()
+					.newBuilder()
+					.addHeader("Content-Type", "application/x-www-form-urlencoded; charset=UTF-8")
+					.addHeader("Authorization", authorizationString)
+					.build();
+				return chain.proceed(request);
+			}
+		});
+
+		final Retrofit retrofit = new Retrofit.Builder().baseUrl("http://web.liferay.com/skinny-web/")
+			.client(builder.build())
 			.addConverterFactory(GsonConverterFactory.create())
 			.build();
 
@@ -29,9 +55,9 @@ public class MainActivity extends AppCompatActivity {
 			@Override
 			public void run() {
 				GitHubService service = retrofit.create(GitHubService.class);
-				Call<List<Event>> call = service.listRepos("octocat");
+				Call<ResponseBody> call = service.events("ddlRecordSetId=70383795");
 				try {
-					Response<List<Event>> response = call.execute();
+					Response<ResponseBody> response = call.execute();
 					EventBus.getDefault().post(response);
 				} catch (IOException e) {
 					Log.e("Agenda", "Don't do this");
