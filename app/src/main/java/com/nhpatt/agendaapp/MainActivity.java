d166c6c32b695f79ef8efab6385e36581b97c89e
Javier Gamarra <nhpatt@gmail.com>
2017-03-10 21:32:44 +0100

    using listener and refactor

--- app/src/main/java/com/nhpatt/agendaapp/MainActivity.java
+++ app/src/main/java/com/nhpatt/agendaapp/MainActivity.java
@@ -1,11 +1,15 @@
 package com.nhpatt.agendaapp;
 
 import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.View;
 import android.widget.ImageView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private boolean favorite;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,7 +18,17 @@ public class MainActivity extends AppCompatActivity {
 
         ImageView favoriteImage = (ImageView) findViewById(R.id.favorite_image);
         favoriteImage.setColorFilter(getResources().getColor(R.color.colorPrimary));
+        favoriteImage.setOnClickListener(this);
 
         Log.d("TAG", "Hi!");
     }
+
+    @Override
+    public void onClick(View v) {
+        ImageView favoriteImage = (ImageView) v;
+        favorite = !favorite;
+        int id = favorite ? R.drawable.favorite : R.drawable.favorite_border;
+        favoriteImage.setImageDrawable(ContextCompat.getDrawable(this, id));
+    }
+
 }
