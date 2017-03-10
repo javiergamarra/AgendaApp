c6529450ff1049aac7f4a537ab70e9b83a426b0f
Javier Gamarra <nhpatt@gmail.com>
2017-03-10 21:04:43 +0100

    changing the color of the favourite button

--- app/src/main/java/com/nhpatt/agendaapp/MainActivity.java
+++ app/src/main/java/com/nhpatt/agendaapp/MainActivity.java
@@ -1,7 +1,10 @@
 package com.nhpatt.agendaapp;
 
+import android.media.Image;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,8 @@ public class MainActivity extends AppCompatActivity {
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
+
+    ImageView favoriteImage = (ImageView) findViewById(R.id.favorite_image);
+    favoriteImage.setColorFilter(getResources().getColor(R.color.colorPrimary));
   }
 }
