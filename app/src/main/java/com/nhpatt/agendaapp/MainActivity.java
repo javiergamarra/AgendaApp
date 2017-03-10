d84a429c7a281c8ea21d60aac44ad104f379f1bc
Javier Gamarra <nhpatt@gmail.com>
2017-03-10 21:07:03 +0100

    use default formatting

--- app/src/main/java/com/nhpatt/agendaapp/MainActivity.java
+++ app/src/main/java/com/nhpatt/agendaapp/MainActivity.java
@@ -1,19 +1,17 @@
 package com.nhpatt.agendaapp;
 
-import android.media.Image;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
+import android.support.v7.app.AppCompatActivity;
 import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
-  @Override
-  protected void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    setContentView(R.layout.activity_main);
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
 
-    ImageView favoriteImage = (ImageView) findViewById(R.id.favorite_image);
-    favoriteImage.setColorFilter(getResources().getColor(R.color.colorPrimary));
-  }
+        ImageView favoriteImage = (ImageView) findViewById(R.id.favorite_image);
+        favoriteImage.setColorFilter(getResources().getColor(R.color.colorPrimary));
+    }
 }
