847e8cbc19b693df4281d3ab7f065f5db52dbc11
Javier Gamarra <nhpatt@gmail.com>
2017-03-11 07:39:16 +0100

    taking a picture

--- app/src/main/java/com/nhpatt/agendaapp/DetailTalkActivity.java
+++ app/src/main/java/com/nhpatt/agendaapp/DetailTalkActivity.java
@@ -1,10 +1,16 @@
 package com.nhpatt.agendaapp;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
-public class DetailTalkActivity extends AppCompatActivity {
+public class DetailTalkActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +24,23 @@ public class DetailTalkActivity extends AppCompatActivity {
 
         TextView time = (TextView) findViewById(R.id.talk_time);
         time.setText(talk.getTime());
+
+        Button addPhotograph = (Button) findViewById(R.id.add_photo);
+        addPhotograph.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        startActivityForResult(intent, 1);
+    }
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        Bundle extras = data.getExtras();
+        Bitmap imageBitmap = (Bitmap) extras.get("data");
+
+        ImageView imageView = (ImageView) findViewById(R.id.photo_taken);
+        imageView.setImageBitmap(imageBitmap);
     }
 
 }
