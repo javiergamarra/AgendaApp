25bd0d2a8f8279d96b212b50caaa6555cba3b94e
Javier Gamarra <nhpatt@gmail.com>
2017-03-11 15:12:01 +0100

    using okhttp

--- app/src/main/java/com/nhpatt/agendaapp/DetailTalkActivity.java
+++ app/src/main/java/com/nhpatt/agendaapp/DetailTalkActivity.java
@@ -5,6 +5,8 @@ import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
+import android.text.Html;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
@@ -12,7 +14,18 @@ import android.widget.TextView;
 
 import com.squareup.picasso.Picasso;
 
+import java.io.IOException;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
 public class DetailTalkActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private static final String TAG = "AgendaApp";
+    private TextView speakerBio;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,13 +43,49 @@ public class DetailTalkActivity extends AppCompatActivity implements View.OnClic
         TextView speaker = (TextView) findViewById(R.id.speaker_name);
         speaker.setText(talk.getSpeaker());
 
+        speakerBio = (TextView) findViewById(R.id.speaker_bio);
+
         Picasso.with(this)
                 .load(talk.getPicture())
                 .resize(200, 200)
                 .into((ImageView) findViewById(R.id.speaker_image));
 
+        loadLastTweet();
+
         Button addPhotograph = (Button) findViewById(R.id.add_photo);
         addPhotograph.setOnClickListener(this);
+    }
+
+    private void loadLastTweet() {
+        OkHttpClient client = new OkHttpClient();
+        Request request = new Request.Builder()
+                .url("http://twitter.com/flipper83/")
+                .build();
+        client.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                final String bio = parse(response.body().string());
+
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        speakerBio.setText(Html.fromHtml(bio));
+                    }
+                });
+            }
+
+            @Override
+            public void onFailure(Call call, IOException e) {
+                Log.e(TAG, e.getMessage());
+            }
+        });
+    }
+
+    private String parse(String string) {
+        int start = string.indexOf(">", string.indexOf("ProfileHeaderCard-bio u-dir"));
+        int end = string.indexOf("ProfileHeaderCard-location");
+
+        return string.substring(start + 1, end);
     }
 
     @Override
