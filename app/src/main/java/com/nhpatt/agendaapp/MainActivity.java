95b25026075519d09d856fc2ae2b03e49dbb0575
Javier Gamarra <nhpatt@gmail.com>
2017-03-11 15:35:15 +0100

    try eventbus

--- app/src/main/java/com/nhpatt/agendaapp/MainActivity.java
+++ app/src/main/java/com/nhpatt/agendaapp/MainActivity.java
@@ -1,6 +1,5 @@
 package com.nhpatt.agendaapp;
 
-import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
@@ -8,10 +7,15 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.View;
 
+import org.greenrobot.eventbus.EventBus;
+import org.greenrobot.eventbus.Subscribe;
+import org.greenrobot.eventbus.ThreadMode;
+
 import java.io.IOException;
-import java.lang.ref.WeakReference;
+import java.util.ArrayList;
 import java.util.List;
 
 import retrofit2.Call;
@@ -20,23 +24,55 @@ import retrofit2.converter.gson.GsonConverterFactory;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
-    private static final Talk TALK_1 = new Talk("09:00", "Reactive Programming and RxJava2", "Jorge Barroso", "https://pbs.twimg.com/profile_images/768819154157731840/4vPCyGBh.jpg");
-    private static final Talk TALK_2 = new Talk("10:00", "Workshop of Android Programming", "Jorge Barroso", "https://pbs.twimg.com/profile_images/768819154157731840/4vPCyGBh.jpg");
-
-    private static List<Talk> talks;
-
-    private static TalkAdapter adapter;
+    private List<Talk> talks;
+    private TalkAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        talks = new ArrayList<>();
+        adapter = new TalkAdapter(this, talks, this);
+        RecyclerView listView = (RecyclerView) findViewById(R.id.list);
+        listView.setLayoutManager(new LinearLayoutManager(this));
+        listView.setAdapter(adapter);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        EventBus.getDefault().register(this);
+
         loadTalks();
     }
 
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        EventBus.getDefault().unregister(this);
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+
+        outState.putSerializable("talks", (ArrayList<Talk>) talks);
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+
+        talks = (List<Talk>) savedInstanceState.getSerializable("talks");
+    }
+
     private void loadTalks() {
-        new Thread(new TalkProcessor(new WeakReference<>(this))).start();
+        if (talks.isEmpty()) {
+            new Thread(new TalkProcessor()).start();
+        }
     }
 
     @Override
@@ -50,10 +86,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         } else {
             talk.setFavorited(!talk.isFavorited());
 
-            getSharedPreferences()
-                    .edit()
-                    .putBoolean(String.valueOf(talk.getId()), talk.isFavorited())
-                    .commit();
+            getSharedPreferences().edit()
+                    .putBoolean(String.valueOf(talk.getId()), talk.isFavorited()).apply();
 
             adapter.notifyDataSetChanged();
         }
@@ -64,11 +98,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     }
 
     private static class TalkProcessor implements Runnable {
-        private final WeakReference<MainActivity> activityWeakReference;
-
-        public TalkProcessor(WeakReference<MainActivity> activityWeakReference) {
-            this.activityWeakReference = activityWeakReference;
-        }
 
         public void run() {
             try {
@@ -81,29 +110,24 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 final Call<List<Talk>> call = service.allTalks();
                 List<Talk> elements = call.execute().body();
 
-                final MainActivity activity = activityWeakReference.get();
-                if (activity != null) {
-
-                    SharedPreferences sharedPreferences = activity.getSharedPreferences();
-                    for (Talk talk : elements) {
-                        talk.setFavorited(sharedPreferences.getBoolean(String.valueOf(talk.getId()), false));
-                    }
-
-                    talks = elements;
-                    activity.runOnUiThread(new Runnable() {
-                        @Override
-                        public void run() {
-                            RecyclerView listView = (RecyclerView) activity.findViewById(R.id.list);
-                            listView.setLayoutManager(new LinearLayoutManager(activity));
-                            adapter = new TalkAdapter(activity, talks, activity);
-                            listView.setAdapter(adapter);
-                        }
-                    });
-                }
+                EventBus.getDefault().post(elements);
 
             } catch (IOException e) {
-                e.printStackTrace();
+                Log.e("TAG", e.getMessage());
             }
         }
+    }
+
+    @Subscribe(threadMode = ThreadMode.MAIN)
+    public void onMessageEvent(List<Talk> elements) {
+
+        SharedPreferences sharedPreferences = getSharedPreferences();
+        for (Talk talk : elements) {
+            talk.setFavorited(sharedPreferences.getBoolean(String.valueOf(talk.getId()), false));
+        }
+
+        talks.clear();
+        talks.addAll(elements);
+        adapter.notifyDataSetChanged();
     }
 }
