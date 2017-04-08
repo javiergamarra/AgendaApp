faca344bf558904ee53983b2420ed6ee00d2de9f
Javier Gamarra <nhpatt@gmail.com>
2017-04-08 07:28:55 +0200

    easy mocito test

---
+++ app/src/test/java/com/nhpatt/agendaapp/MockitoTest.java
@@ -0,0 +1,55 @@
+package com.nhpatt.agendaapp;
+
+import org.junit.Test;
+
+import java.util.LinkedList;
+import java.util.List;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+public class MockitoTest {
+
+    @Test
+    public void addition_isCorrect() throws Exception {
+        LinkedList mockedList = mock(LinkedList.class);
+
+        //stubbing
+        when(mockedList.get(0)).thenReturn("first");
+        when(mockedList.get(1)).thenThrow(new RuntimeException());
+
+        //following prints "first"
+        System.out.println(mockedList.get(0));
+        System.out.println(mockedList.get(0));
+
+        //following throws runtime exception
+        try {
+            System.out.println(mockedList.get(1));
+        } catch (Exception e) {
+
+        }
+
+        //following prints "null" because get(999) was not stubbed
+        System.out.println(mockedList.get(999));
+
+        //Although it is possible to verify a stubbed invocation, usually it's just redundant
+        //If your code cares what get(0) returns, then something else breaks (often even before verify() gets executed).
+        //If your code doesn't care what get(0) returns, then it should not be stubbed. Not convinced? See here.
+        verify(mockedList, times(2)).get(0);
+    }
+
+    @Test
+    public void mockTest() {
+        List mockedList = mock(List.class);
+
+        //using mock object
+        mockedList.add("one");
+        mockedList.clear();
+
+        //verification
+        verify(mockedList).add("one");
+        verify(mockedList).clear();
+    }
+}
