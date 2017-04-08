2b0e888b5787a9923835399f7528ff6968e95cbf
Javier Gamarra <nhpatt@gmail.com>
2017-04-08 07:49:14 +0200

    improve espresso test

--- app/src/androidTest/java/com/nhpatt/agendaapp/EspressoTest.java
+++ app/src/androidTest/java/com/nhpatt/agendaapp/EspressoTest.java
@@ -21,11 +21,8 @@ import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.action.ViewActions.closeSoftKeyboard;
 import static android.support.test.espresso.action.ViewActions.replaceText;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
-import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.isEnabled;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
-import static android.support.test.espresso.matcher.ViewMatchers.withText;
-import static org.hamcrest.Matchers.allOf;
 
 @LargeTest
 @RunWith(AndroidJUnit4.class)
@@ -36,52 +33,12 @@ public class EspressoTest {
 
     @Test
     public void espressoTest() {
-        ViewInteraction appCompatEditText = onView(
-                allOf(childAtPosition(
-                        childAtPosition(
-                                withId(R.id.name_edit),
-                                0),
-                        0),
-                        isDisplayed()));
+        ViewInteraction appCompatEditText = onView(childAtPosition(childAtPosition(withId(R.id.name_edit), 0), 0));
         appCompatEditText.perform(replaceText("Hola!"), closeSoftKeyboard());
 
-        ViewInteraction appCompatEditText4 = onView(
-                allOf(childAtPosition(
-                        childAtPosition(
-                                withId(R.id.description_edit),
-                                0),
-                        0),
-                        isDisplayed()));
-        appCompatEditText4.perform(click());
-
-        ViewInteraction appCompatEditText5 = onView(
-                allOf(childAtPosition(
-                        childAtPosition(
-                                withId(R.id.description_edit),
-                                0),
-                        0),
-                        isDisplayed()));
-        appCompatEditText5.perform(replaceText("Adios!"), closeSoftKeyboard());
-
-        ViewInteraction appCompatButton = onView(
-                allOf(withId(R.id.submit_button), withText("Add Talk"),
-                        childAtPosition(
-                                childAtPosition(
-                                        withId(R.id.card_view),
-                                        0),
-                                2),
-                        isDisplayed()));
-        appCompatButton.perform(click());
-
-        ViewInteraction button = onView(
-                allOf(withId(R.id.submit_button),
-                        childAtPosition(
-                                childAtPosition(
-                                        withId(R.id.card_view),
-                                        0),
-                                2),
-                        isDisplayed()));
-        button.check(matches(isEnabled()));
+        ViewInteraction submitButton = onView(withId(R.id.submit_button));
+        submitButton.perform(click());
+        submitButton.check(matches(isEnabled()));
     }
 
     private static Matcher<View> childAtPosition(
