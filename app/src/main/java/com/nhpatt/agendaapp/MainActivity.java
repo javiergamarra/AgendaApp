cf50d80aa2a10b6163ddde77bbd63db1a30ee5ce
Javier Gamarra <nhpatt@gmail.com>
2017-04-08 02:01:14 +0200

    transition manager

--- app/src/main/java/com/nhpatt/agendaapp/MainActivity.java
+++ app/src/main/java/com/nhpatt/agendaapp/MainActivity.java
@@ -1,12 +1,16 @@
 package com.nhpatt.agendaapp;
 
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.transition.TransitionManager;
 import android.view.View;
+import android.view.ViewGroup;
 
 import com.nhpatt.agendaapp.fragments.ExampleFragment;
 
@@ -22,6 +26,8 @@ public class MainActivity extends AbstractActivityWithPresenter<TalkPresenter> i
     @BindView(R.id.list)
     RecyclerView listView;
     private TalkAdapter adapter;
+    @BindView(R.id.fab)
+    FloatingActionButton fab;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -66,6 +72,15 @@ public class MainActivity extends AbstractActivityWithPresenter<TalkPresenter> i
                 adapter.getTalks().remove(0);
             }
             adapter.notifyDataSetChanged();
+
+            ViewGroup viewById = (ViewGroup) findViewById(R.id.main_content);
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+                TransitionManager.beginDelayedTransition(viewById);
+
+
+                View child = viewById.findViewById(R.id.fab);
+                child.setVisibility(View.INVISIBLE);
+            }
         }
     }
 
